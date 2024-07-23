fb25f4929b7c4d50de48679805724cfda26831c1
Lawal Toheeb <lawaltoheeb36@gmail.com>
2024-07-23 14:13:07 +0100

    first commit

---
+++ src/test/java/com/jumong/studentmanagementsystem/StudentManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jumong.studentmanagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentManagementSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
